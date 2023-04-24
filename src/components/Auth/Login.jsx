30625d609a54bfe19130f17a5ca4ae2d99181e02
AbdullahWB <abdullah917828@gmail.com>
2023-04-25 00:27:09 +0600

    wrong or valide pass and input validation

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -35,14 +35,14 @@ const Login = () => {
   return <div className="mt-36">
     <form onSubmit={handleSubmit} className="bg-gray-200 p-10 rounded-lg">
       <div className="relative z-0 w-full mb-6 group">
-        <input value={email} onChange={handleEmail} type="email" name="email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <input value={email} onChange={handleEmail} type="email" name="email" id="floating_email" className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none dark:text-white focus:outline-none focus:ring-0 peer ${email ? emailErr ? "border-red-500" : "border-green-500" : "border-gray-300 focus:border-blue-600 dark:focus:border-blue-500 dark:border-gray-600"}`} placeholder=" " required />
         <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
         {
           emailErr && <span className="error">{emailErr}</span>
         }
       </div>
       <div className="relative z-0 w-full mb-6 group">
-        <input value={password} onChange={handlePassword} type="password" name="password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <input value={password} onChange={handlePassword} type="password" name="password" id="floating_password" className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none dark:text-white focus:outline-none focus:ring-0  peer ${password ? passwordErr ? "border-red-500" : "border-green-500" : "border-gray-300 dark:focus:border-blue-500 focus:border-blue-600 dark:border-gray-600"}`} placeholder=" " required />
         <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
         {
           passwordErr && <span className="error">{passwordErr}</span>
